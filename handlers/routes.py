476cde6d126c94c76e9dbfd14dd586ee53efcdff
Erdeni <215928@edu.fa.ru>
2023-06-26 01:14:45 +0300

    try2

--- handlers/routes.py
+++ handlers/routes.py
@@ -1,4 +1,4 @@
-from flask import request, render_template
+from flask import request, abort, Response
 from modules import modules
 import telebot
 import time
@@ -6,16 +6,26 @@ import os
 
 
 def configure_routes(app, bot):
-    @app.route("/")
+    @app.route("/", methods = ['POST', 'GET'])
     def index():
-        bot.remove_webhook()
-        time.sleep(1)
-        bot.set_webhook(url=os.getenv("URL"))
-        return "I'm alive"
+        # bot.remove_webhook()
+        # time.sleep(1)
+        # bot.set_webhook(url=os.getenv("URL"))
+        if request.headers.get('content-type')=='application/json':
+            update = telebot.types.Update.de_json(
+                request.stream.read().decode("utf-8"))
+            bot.process_new_updates([update])
+            return "I'm alive"
+        else: 
+            abort(403)
+        if request.method == 'POST':
+            return Response('ok', status=200)
+        else:
+            return ""
 
-    @app.route('/webhook', methods=['POST'])
-    def webhook():
-        update = telebot.types.Update.de_json(
-            request.stream.read().decode("utf-8"))
-        bot.process_new_updates([update])
-        return "ok", 200
+    # @app.route('/webhook', methods=['POST'])
+    # def webhook():
+    #     update = telebot.types.Update.de_json(
+    #         request.stream.read().decode("utf-8"))
+    #     bot.process_new_updates([update])
+    #     return "ok", 200
