9a2960efaa313480d87b2c520591d8d456f3c0da
Erdeni <215928@edu.fa.ru>
2023-06-26 02:01:19 +0300

    changes

--- handlers/routes.py
+++ handlers/routes.py
@@ -9,17 +9,13 @@ def configure_routes(app, bot):
     
     @app.route("/")
     def index():
+        bot.remove_webhook()
+        time.sleep(1)
+        bot.set_webhook(url=os.getenv("URL"))
         return "I'm alive"
     
     @app.route("/" + str(os.getenv("SECRET")), methods=["POST"])
     def webhook():    
         update = telebot.types.Update.de_json(request.stream.read().decode("utf-8"))
         bot.process_new_updates([update])
-        return "ok", 200
-
-    # @app.route('/webhook', methods=['POST'])
-    # def webhook():
-    #     update = telebot.types.Update.de_json(
-    #         request.stream.read().decode("utf-8"))
-    #     bot.process_new_updates([update])
-    #     return "ok", 200
+        return "ok", 201
